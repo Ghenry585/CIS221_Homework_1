c6f19078937f5a38bf67a485d2d89bebad0548ba
Gary Henry <93809854+Ghenry585@users.noreply.github.com>
2022-01-31 14:19:51 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* Gary Henry
+CIS221
+Tuesdays and Thrusdays (2:10pm to 4:00pm)
+Professor: Mr.Swinarski */
+
+public class Main {
+
+        String A = "My name is Gary Henry";
+        String B = "and this is class CIS221 and I've learned how to manipulate data!";
+        int C = 219;
+        int D = 2;
+
+}
---
+++ Second.java
@@ -0,0 +1,15 @@
+/* Gary Henry
+CIS221
+Tuesdays and Thrusdays (2:10pm to 4:00pm)
+Professor: Mr.Swinarski */
+
+public class Second {
+    public static void main(String[] args) {
+
+        Main myobj = new Main();
+        System.out.println(myobj.A + " " + myobj.B);
+        System.out
+                .println("My first class was CIS " + myobj.C + "." + " Now im taking CIS " + (myobj.C + myobj.D) + ".");
+
+    }
+}
